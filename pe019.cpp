4a4d5a35c3363d7a32eec6d6893fbbf57c4abf33
sokampdx <sokam@pdx.edu>
2014-07-29 16:27:42 -0700

    pe19 completed

--- pe019.cpp
+++ pe019.cpp
@@ -8,15 +8,15 @@
 using namespace std;
 
 // global constant
-const int DAYINYEAR = 365;
-const int LEAPYEAR = 366;
+const int REGULAR[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+const int LEAP[] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 // create date type
 typedef struct {
 	int year;
 	int month;
 	int day;
-	int dayoftheWeek;	// 0 = Sunday
+	int dayoftheWeek;	// 1 = Monday
 } Date;
 
 // global variable
@@ -34,38 +34,44 @@ void initialize() {
 // calculate the number of days in a range of year
 int calculateDay(int startYear, int endYear) {
 	int totalDay = 0;
+	int count = 0;
+	int dayoftheWeek = ( seedDate.dayoftheWeek + 365 ) % 7 ;
 
-	for (int i = startYear; i <= endYear; ++i) {
-		if (((i % 400) == 0) || ((i % 4) != 0))
-			totalDay += DAYINYEAR;
-		else
-			totalDay += LEAPYEAR;
+//	if (dayoftheWeek == 0)
+//		++count;
+
+	for (int year = startYear; year <= endYear; ++year) {
+		for (int month = 1; month <= 12; ++month) {
+			if (dayoftheWeek == 0)
+				++count;
+
+			if (((year % 400) == 0) || ((year % 4) != 0))
+				totalDay = REGULAR[month];
+			else
+				totalDay = LEAP[month];
+
+			dayoftheWeek = (totalDay + dayoftheWeek) % 7;	
+			totalDay = 0;
+		}
 	}
-	return totalDay;
+
+	//if (dayoftheWeek == 0)
+	//	--count;
+
+	return count;
 	
 }
-
-// find 1/1/year day of the week using seed
-int findDayoftheWeek(int year) {
-	int numDay = 0;
-	int result = 0;
-
-	numDay = calculateDay(seedDate.year, year);
 
 
 // main program
 int main () 
 {
 	int result = 0;
-	int startDayoftheWeek = 0;
-	int numDay = 0;
 
 	initialize();
 
-	numDay = calculateDay(1901, 2000);
-	startDayoftheWeek = findDayoftheWeek(1901);
-	
-	cout << "Number of days = " << numDay << endl;
+	result = calculateDay(1901, 2000);
+
 	cout << "Result = " << result << endl;	
 
 	return 0;
