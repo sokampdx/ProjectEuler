fff02e7ed48816732a0c9863a01e91913c91bc64
sokampdx <sokam@pdx.edu>
2015-01-20 21:59:53 -0800

    put better function name in prob 46

--- pe046.cpp
+++ pe046.cpp
@@ -41,14 +41,102 @@ void printPrime() {
 	}
 }
 
+bool isPerfectSquare(unsigned long x) {
+	return x == round(sqrt(x)) * round(sqrt(x));
+}
+
+unsigned long nextPrimeBelow(unsigned long n) {
+	unsigned long p = n - 2;
+	while (!prime[p]) {
+		p-=2;
+	}
+	return p;
+}
+
+unsigned long nextCompositeAbove(unsigned long n) {
+	unsigned long x = n + 2;
+	while (prime[x]) {
+		x+=2;
+	}
+	return x;
+}
+
+unsigned long findSmallest() {
+	unsigned long answer = 0;
+	unsigned long n = 9;
+	unsigned long p = 3;
+	bool found = false;
+
+	while (!answer && n < MAX) {
+		p = nextPrimeBelow(n);
+		found = false;
+		while (!found && p > 2) {
+			if (isPerfectSquare( (n-p) / 2)) {
+				found = true;
+			}
+			p = nextPrimeBelow(p);
+		}
+		if (!found) {
+			answer = n;
+		}
+		n = nextCompositeAbove(n);
+	}
+	return answer;
+}	
+
+		
+
+void test() {
+	unsigned long x = 1;
+	cout << isPerfectSquare(x) << endl;
+	cout << "Expect: 1" << endl;
+	x = 4;
+	cout << isPerfectSquare(x) << endl;
+	cout << "Expect: 1" << endl;
+	x = 122;
+	cout << isPerfectSquare(x) << endl;
+	cout << "Expect: 0" << endl;
+	x = 120;
+	cout << isPerfectSquare(x) << endl;
+	cout << "Expect: 0" << endl;
+
+	unsigned long p = 384547;
+	cout << prime[p] << endl;
+	cout << "Expect: 1" << endl;
+	
+	x = p;
+	cout << nextPrimeBelow(x) << endl;
+	cout << "Expect: 384533" << endl;
+	x = 121;
+	cout << nextPrimeBelow(x) << endl;
+	cout << "Expect: 113" << endl;
+	x = 27;
+	cout << nextPrimeBelow(x) << endl;
+	cout << "Expect: 23" << endl;
+	
+	x = 119;
+	cout << nextCompositeAbove(x) << endl;
+	cout << "Expect: 121" << endl;
+	x = 3;
+	cout << nextCompositeAbove(x) << endl;
+	cout << "Expect: 9" << endl;
+	x = 7545;
+	cout << nextCompositeAbove(x) << endl;
+	cout << "Expect: 7551" << endl;
+		
+}
+
 
 // main program
 int main () 
 {
 	initialize();
 	primeSieve();
-	printPrime();
+	//printPrime();
 
+	//test();	
+
+	cout << "The result = " << findSmallest() << endl;
 	return 0;
 }
 
