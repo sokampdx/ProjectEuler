0eef0351115a7d508d41305b9cbff776d0a50400
sokampdx <sokam@pdx.edu>
2015-01-20 17:45:31 -0800

    start problem 45

---
+++ pe045.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <sstream>
+
+using namespace std;
+
+// global constant
+
+// global variable
+
+
+// main program
+int main () 
+{
+	unsigned long result = 0;
+	
+	cout << "The result = " << result << endl;
+
+	return 0;
+}
+
