a5e0a63456489dfd089ac054e68b9d043e379d82
sokampdx <sokam@pdx.edu>
2015-01-28 14:50:10 -0800

    start p48

---
+++ pe048.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+// global constant
+const unsigned long MAX = 10000000000;
+
+
+
+
+// main program
+int main () 
+{
+
+	unsigned long result = MAX;
+	cout << "The result = " << result << endl;
+
+	return 0;
+}
+
