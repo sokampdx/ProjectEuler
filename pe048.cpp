5a1f2e0373d2d5e8193258a0a714ccfd83dcd565
sokampdx <sokam@pdx.edu>
2015-01-28 16:51:56 -0800

    completed prob 48

--- pe048.cpp
+++ pe048.cpp
@@ -3,11 +3,76 @@
 #include <cmath>
 #include <string>
 #include <cstring>
+#include <climits>
+
+typedef unsigned long long ULL;
 
 using namespace std;
 
 // global constant
-const unsigned long MAX = 10000000000;
+const ULL TENDIGIT = 10000000000LL;
+const ULL MAX = 1000LL;
+
+ULL power(ULL x, ULL p) {
+	if (p == 1LL)
+		return x;
+	
+	ULL result = power(x, p/2) % TENDIGIT;
+	// still overflow for unsigned long long
+	result = (result * result) % TENDIGIT;
+	//cout << p << ":" << p/2 << ":" << p%2 << ":" << result << endl;
+	
+	if (p % 2LL == 0LL) {
+		return result;
+	}
+
+	return (result * x) % TENDIGIT;
+}
+
+
+ULL power2(ULL x, ULL p) {
+	ULL result = x;
+	for (ULL i = 0; i < p-1; ++i) {
+		result = (result * x) % TENDIGIT;
+	}
+
+	return result;
+}
+
+
+ULL selfPower(ULL x) {
+	return power2(x, x) % TENDIGIT;
+}
+
+
+
+ULL lastTenDigit() {
+	ULL result = 0;
+	for (ULL i = 1; i <= MAX; ++i) {
+		result = (result + selfPower(i)) % TENDIGIT;
+	}
+
+	return result;
+}
+
+void test() {
+	ULL x = 3;
+	cout << power(x, x) << endl;
+	cout << selfPower(x) << endl;
+	x = 999;
+	cout << power(x, x) << endl;
+	cout << selfPower(x) << endl;
+	
+	ULL y = 9999999999 * 9999999999;
+	cout << y << endl;
+	cout << power(y, 2) << endl;
+	cout << power2(y, 2) << endl;
+
+	cout << LLONG_MAX << endl;
+	cout << LONG_MAX << endl;
+
+	cout << 999/2 << endl;
+}
 
 
 
@@ -15,8 +80,8 @@ const unsigned long MAX = 10000000000;
 // main program
 int main () 
 {
-
-	unsigned long result = MAX;
+	//test();
+	ULL result = lastTenDigit();
 	cout << "The result = " << result << endl;
 
 	return 0;
