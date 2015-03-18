bb1a343e9547cac9a58d677b898469d0226b8dc8
sokampdx <sokam@pdx.edu>
2015-03-18 00:20:14 -0700

    tested 4 digits number without solutions

--- pe051.cpp
+++ pe051.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 const dtype LIMIT = 1000000;
 const dtype SQRTLIMIT = (dtype) sqrt(LIMIT) + 1;
-const dtype MAX = LIMIT / 2 + 1;
+const dtype FOUND = 8;
 
 bool prime[LIMIT];
 
@@ -69,6 +69,49 @@ dtype nextPrimeBelow(dtype number) {
 	return value;
 }
 
+dtype findIn4digits() {
+	dtype result = 0;
+	dtype digit = 1;
+	bool isFound = false;
+	
+	while ((digit < 8) && (!isFound)) {
+		dtype count = 0;
+		dtype min = 0;
+		dtype current = 0;
+		dtype value = 0;	
+	
+		for (dtype i = 1; i < 10; ++i) {
+			current += 1110;
+			value = current + digit;
+			
+			if (isPrime(value)) {
+				cout << digit << ":" << value << endl;
+				if (count == 0) {
+					min = value;
+				} 
+				++count;
+			}
+		}
+
+		if (count >= FOUND) {
+			isFound = true;
+			result = min;
+		}
+		digit+=6;
+	}
+	return result;
+}
+
+
+
+dtype find8PrimeFamily() {
+	dtype result = 0;
+
+	result = findIn4digits();
+
+	cout << result << endl;
+	return result;
+}
 
 void test() {
 
@@ -84,7 +127,7 @@ int main ()
 	//printPrime();
 
 	//test();
-
+	result = find8PrimeFamily();
 	cout << "The result is: "  << result << endl;
 
 	return 0;
