43876d0666a9d9cd6408e21281ee0d0b6e54e510
sokampdx <sokam@pdx.edu>
2015-04-06 22:04:31 -0700

    refactor pe001

--- pe001.cpp
+++ pe001.cpp
@@ -1,36 +1,24 @@
 #include <iostream> 
+#include <sstream>
+#include "lib/summation.h"
  
 using namespace std; 
  
-int main() 
-{ 
-   int N = 1000; 
-   int i, j, k; 
-   int sum = 0; 
-    
-   // Use summation from 1 to N 
-   // O(1) 
-   i = ((N - 1) / 3); 
-   j = ((N - 1) / 5); 
-   k = ((N - 1) / 15); 
-    
-   sum = sum + (3 * ((i + 1) * i / 2)); 
-   sum = sum + (5 * ((j + 1) * j / 2)); 
-   sum = sum - (15 * ((k + 1) * k / 2)); 
-    
-   cout << endl << "The result = " << sum << endl; 
- 
- 
-   //brute force algorithm 
-   //O(n) 
-   sum = 0;      
-   for (i = 1; i < N; i++) 
-     if (!(i % 3) || !(i % 5)) 
-       sum = sum + i; 
- 
-   cout << endl << "The result = " << sum << endl; 
-    
-   return 0; 
-} 
- 
- 
+int main(int argc, char *argv[]) {
+	if (argc == 2) {
+		istringstream ss(argv[1]);
+		int n;
+		if (!(ss >> n)) {
+			cerr << "Invalid number" << argv[1] << endl;
+      	} else {
+			Summation<int> sum(n);
+			cout << "The result is = " << sum.getResult() << endl;
+		}
+	} else {
+		cout << "usage: " << argv[0] << " <input> " << endl;
+	}
+
+	return 0;
+}
+  
+
