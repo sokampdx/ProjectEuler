74e7506e46e08adc48a03bf4ad54e8d65dd54b8d
sokampdx <sokam@pdx.edu>
2015-04-06 22:31:55 -0700

    refactor pe001

--- pe001.cpp
+++ pe001.cpp
@@ -3,6 +3,31 @@
 #include "lib/summation.h"
  
 using namespace std; 
+
+int findN(int max, int divisor) {
+	return ((max - 1) / divisor);
+}
+
+int findSolution(int max) {
+	int i, j, k;
+	int sum = 0;
+
+	i = findN(max, 3);
+	j = findN(max, 5);
+	k = findN(max, 15);
+
+	Summation<int> summation(i);
+	sum += (3 * summation.getResult());
+
+	summation.setResult(j);
+	sum += (5 * summation.getResult());
+
+	summation.setResult(k);
+	sum -= (15 * summation.getResult());
+
+	return sum;
+}
+
  
 int main(int argc, char *argv[]) {
 	if (argc == 2) {
@@ -11,8 +36,7 @@ int main(int argc, char *argv[]) {
 		if (!(ss >> n)) {
 			cerr << "Invalid number" << argv[1] << endl;
       	} else {
-			Summation<int> sum(n);
-			cout << "The result is = " << sum.getResult() << endl;
+			cout << "The result is = " << findSolution(n) << endl;
 		}
 	} else {
 		cout << "usage: " << argv[0] << " <input> " << endl;
